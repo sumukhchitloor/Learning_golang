b7e1ab4bd92a550faa0c87f757eeddd365f0f7a6
sumukhchitloor <sumukhchitloor18@gmail.com>
2025-09-01 12:18:07 +0530

    idk

---
+++ memory management in go/condition valriables and semaphores/stingy-and-spend-made-better.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+func stingy(money *int, mu *sync.Mutex) {
+	for i := 0; i < 1000000; i++ {
+		mu.Lock()
+		*money += 10
+		mu.Unlock()
+	}
+	fmt.Println("stingy done")
+}
+
+func spendy(money *int, mu *sync.Mutex) {
+	for i := 0; i < 200000; i++ {
+		mu.Lock()
+		*money -= 50
+		if *money < 0 {
+			fmt.Println("overspent!", *money)
+			os.Exit(1)
+		}
+		mu.Unlock()
+	}
+	fmt.Println("spendy done")
+}
+
+func main() {
+	mu := sync.Mutex{}
+	money := 100
+	go stingy(&money, &mu)
+	go spendy(&money, &mu)
+	fmt.Println("waiting...")
+	time.Sleep(2 * time.Second)
+
+	mu.Lock()
+	fmt.Println("money:", money)
+	mu.Unlock()
+
+}
